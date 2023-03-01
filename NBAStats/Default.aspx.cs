af3371af97fb199ba0269d00fd6d79d25fa48240
Ricardo Queiroz <ricardo.queiroz@klassmatt.com>
2023-03-01 14:08:31 -0300

    Ajuste em bug que acontecia quando era informado uma quantia maior de jogos do que o jogador tinha jogado.

--- NBAStats/Default.aspx.cs
+++ NBAStats/Default.aspx.cs
@@ -194,7 +194,9 @@ namespace NBAStats
             Jogador.Partidas.Clear();
             Jogador.PartidasContraAdv.Clear();
 
-            for (int i = 0; i < QuantJogos; i++)
+            var quant = JogadorStats.Count() < QuantJogos ? JogadorStats.Count() : QuantJogos;
+
+            for (int i = 0; i < quant; i++)
             {
                 var node = JogadorStats[i];
 
@@ -204,7 +206,7 @@ namespace NBAStats
 
                 if (!string.IsNullOrEmpty(txtMinMinutos.Text) && Convert.ToInt32(nodeMinutos.InnerText.Substring(0, nodeMinutos.InnerText.IndexOf(":"))) < Convert.ToInt32(txtMinMinutos.Text))
                 {
-                    QuantJogos++;
+                    quant++;
                     continue;
                 }
 
